6881c72cbe24c56429523317b423a44946e6cdc4
Brian Alexander <brianalexander348@gmail.com>
2025-10-18 15:24:15 +0700

    add userContext for tracking user

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { UserProvider } from "../src/context/userContext";
 
 export default function Layout() {
   return (
-    <>
+    <UserProvider>
       <StatusBar style="dark" />
       <Stack
         screenOptions={{
@@ -12,9 +13,10 @@ export default function Layout() {
           animation: "fade"
         }}
       />
-    </>
+    </UserProvider>
   );
 }
+
 
 
 
